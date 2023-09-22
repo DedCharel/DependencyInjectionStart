09f6d34316697a869e77360ed21bb623b0e3118e
Daniil Shcherbakov <Danvikmail@gmail.com>
2023-09-22 13:38:55 +0300

    Inject dependency without Dagger

--- app/src/main/java/com/example/dependencyinjectionstart/example1/Activity.kt
+++ app/src/main/java/com/example/dependencyinjectionstart/example1/Activity.kt
@@ -1,13 +1,10 @@
 package com.example.dependencyinjectionstart.example1
 
 class Activity {
-    val monitor = Monitor()
-    val keyboard = Keyboard()
-    val mouse = Mouse()
-    val computerTower = ComputerTower(
-        Storage(),
-        Memory(),
-        Processor()
-    )
-    val computer = Computer(monitor, computerTower, keyboard, mouse)
+    lateinit var computer: Computer
+    lateinit var keyboard: Keyboard
+
+    init {
+        Component().inject(this)
+    }
 }
