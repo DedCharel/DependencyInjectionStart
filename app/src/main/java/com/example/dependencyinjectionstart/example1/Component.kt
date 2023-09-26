4a630a0865f6b73ad509a812595b5836aafa4b8f
Daniil Shcherbakov <Danvikmail@gmail.com>
2023-09-26 10:50:10 +0300

    inject the dependency using Dagger 2 (fun inject)

--- app/src/main/java/com/example/dependencyinjectionstart/example1/Component.kt
+++ app/src/main/java/com/example/dependencyinjectionstart/example1/Component.kt
@@ -14,7 +14,7 @@ class Component {
     }
 
     fun inject(activity: Activity){
-        val computer = getComputer()
-        val keyboard = Keyboard()
+
+        activity.keyboard = Keyboard()
     }
 }
