6e2ab2b77dfc9c4e26fd42869dbe05ddd17e69c2
Daniil Shcherbakov <Charel@bk.ru>
2024-10-30 14:46:01 +0300

    refactoring example2

--- app/src/main/java/com/example/dependencyinjectionstart/example2/presentation/MainActivity.kt
+++ app/src/main/java/com/example/dependencyinjectionstart/example2/presentation/MainActivity.kt
@@ -5,9 +5,12 @@ import android.os.Bundle
 import com.example.dependencyinjectionstart.R
 import com.example.dependencyinjectionstart.example1.Activity
 
+lateinit var viewModel: ExampleViewModel
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        viewModel.method()
     }
 }
