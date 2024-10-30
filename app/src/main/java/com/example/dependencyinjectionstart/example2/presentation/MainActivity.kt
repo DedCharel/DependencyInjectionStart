5ace4bb3ee2c00b0b68d3eb13a027bd28d47a9d9
Daniil Shcherbakov <Charel@bk.ru>
2024-10-30 14:35:38 +0300

    example2

--- app/src/main/java/com/example/dependencyinjectionstart/example2/presentation/MainActivity.kt
+++ app/src/main/java/com/example/dependencyinjectionstart/example2/presentation/MainActivity.kt
@@ -1,16 +1,13 @@
-package com.example.dependencyinjectionstart
+package com.example.dependencyinjectionstart.example2.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.dependencyinjectionstart.R
 import com.example.dependencyinjectionstart.example1.Activity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Activity().keyboard.toString()
-        Activity().mouse.toString()
-        Activity().monitor.toString()
-        Activity().computer.toString()
     }
 }
