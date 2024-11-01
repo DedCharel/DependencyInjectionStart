6142b575e653f76003fed7e6c45f840d80fbcc75
Daniil Shcherbakov <Charel@bk.ru>
2024-11-01 11:56:56 +0300

    ViewModelFactory. Use collections ViewModels(Map<String, ViewModel>) for create viewModel

--- app/src/main/java/com/example/dependencyinjectionstart/example2/presentation/ViewModelFactory.kt
+++ app/src/main/java/com/example/dependencyinjectionstart/example2/presentation/ViewModelFactory.kt
@@ -6,12 +6,9 @@ import com.example.dependencyinjectionstart.example2.domain.ExampleUseCase
 import javax.inject.Inject
 
 class ViewModelFactory @Inject constructor(
-    private val exampleUseCase: ExampleUseCase
+    private val viewModels: @JvmSuppressWildcards Map<String, ViewModel>
 ): ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        if (modelClass == ExampleViewModel::class.java) {
-            return ExampleViewModel(exampleUseCase) as T
-        }
-        throw RuntimeException("Unknown viewmodel class $modelClass")
+       return viewModels[modelClass.simpleName] as T
     }
 }
