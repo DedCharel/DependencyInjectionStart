b1d6b5ed67731fd4d5e6a5ebd2618766c751e52a
Daniil Shcherbakov <Charel@bk.ru>
2024-11-04 17:10:38 +0300

    Use @Subcomponent

--- app/src/main/java/com/example/dependencyinjectionstart/example2/di/ApplicationComponent.kt
+++ app/src/main/java/com/example/dependencyinjectionstart/example2/di/ApplicationComponent.kt
@@ -10,34 +10,12 @@ import dagger.BindsInstance
 import dagger.Component
 
 @ApplicationScope
-@Component(modules = [DataModule::class, DomainModule::class, ViewModelModule::class])
+@Component(modules = [DataModule::class, DomainModule::class])
 interface ApplicationComponent {
 
-    fun getViewModel(): ExampleViewModel
-
-    fun getDatabase(): ExampleDatabase
-
-    fun getApiService(): ExampleApiService
-
-    fun inject(activity: MainActivity)
-
-    fun inject(activity: MainActivity2)
+    fun activityComponentFactory(): ActivityComponent.Factory
 
 
-    //***** Если мы хотим использовать Bulder *****
-//    @Component.Builder
-//    interface ApplicationComponentBuilder {
-//
-//        @BindsInstance
-//        fun context(context: Context): ApplicationComponentBuilder
-//
-//        @BindsInstance
-//        fun timeMillis(timeMillis: Long): ApplicationComponentBuilder
-//
-//        fun build(): ApplicationComponent
-//    }
-
-    //***** Если мы хотим использовать Factory *****
     @Component.Factory
     interface ApplicationComponentFactory {
 
